dd0b46937663f5c817507dd1718ee3d8e4baf362
Huy Bach <lhbach2002ct@gmail.com>
2023-07-06 17:21:39 +0700

    update loading

--- src/containers/public/SearchAll.js
+++ src/containers/public/SearchAll.js
@@ -6,107 +6,116 @@ import ListSongItem from "../../components/ListSongItem";
 import SectionItem from "../../components/SectionItem";
 import Artist from "../../components/Artist";
 import {Link} from "react-router-dom";
+import LoadingData from "../../components/LoadingData";
 
 const SearchAll = () => {
   const {searchData} = useSelector((state) => state.music);
-  console.log("searchData: ", searchData);
 
   return (
-    <div className="w-full flex flex-col px-[59px]">
-      <div className="flex flex-col gap-6">
-        <h3 className="text-[20px] font-bold">Nổi Bật</h3>
-        <div className="flex gap-8">
-          {searchData?.top && (
-            <Link
-              to={searchData?.top?.link}
-              className="flex items-center flex-1 gap-8 p-[10px] bg-searchItem hover:bg-bgIcon rounded-lg cursor-pointer"
-            >
-              <img
-                src={searchData?.top?.thumbnail}
-                alt=""
-                className={`w-[84px] h-[84px] object-cover ${
-                  searchData?.top?.objectType === "artist"
-                    ? "rounded-full"
-                    : "rounded-lg"
-                }`}
-              />
-              <div className="flex flex-col">
-                <span className="text-[12px] mb-1 text-textGrey">
-                  {(searchData?.top?.objectType === "artist" && "Nghệ sĩ") ||
-                    (searchData?.top?.objectType === "song" && "Bài hát")}
-                </span>
-                <span className="text-[14px] font-semibold">
-                  {searchData?.top?.title || searchData?.top?.name}
-                </span>
-                <span className="text-[12px] mt-1 text-textGrey">
-                  {(searchData?.top?.objectType === "artist" &&
-                    formatNumber(searchData?.artists[0]?.totalFollow) +
-                      " quan tâm") ||
-                    (searchData?.top?.objectType === "song" &&
-                      searchData?.top?.artistsNames)}
-                </span>
-              </div>
-            </Link>
-          )}
-          {searchData?.songs
-            ?.filter((item, index) =>
-              [...Array(2).keys()].some((i) => i === index)
-            )
-            ?.map((item) => (
-              <div
-                key={item.encodeId}
-                className="flex-1 bg-searchItem flex items-center rounded-lg hover:bg-bgIcon"
-              >
-                <SongItem
+    <>
+      {searchData?.top?.thumbnail && (
+        <div className="w-full flex flex-col px-[59px]">
+          <div className="flex flex-col gap-6">
+            <h3 className="text-[20px] font-bold">Nổi Bật</h3>
+            <div className="flex gap-8">
+              {searchData?.top && (
+                <Link
+                  to={searchData?.top?.link}
+                  className="flex items-center flex-1 gap-8 p-[10px] bg-searchItem hover:bg-bgIcon rounded-lg cursor-pointer"
+                >
+                  <img
+                    src={searchData?.top?.thumbnail}
+                    alt=""
+                    className={`w-[84px] h-[84px] object-cover ${
+                      searchData?.top?.objectType === "artist"
+                        ? "rounded-full"
+                        : "rounded-lg"
+                    }`}
+                  />
+                  <div className="flex flex-col">
+                    <span className="text-[12px] mb-1 text-textGrey">
+                      {(searchData?.top?.objectType === "artist" &&
+                        "Nghệ sĩ") ||
+                        (searchData?.top?.objectType === "song" && "Bài hát")}
+                    </span>
+                    <span className="text-[14px] font-semibold">
+                      {searchData?.top?.title || searchData?.top?.name}
+                    </span>
+                    <span className="text-[12px] mt-1 text-textGrey">
+                      {(searchData?.top?.objectType === "artist" &&
+                        formatNumber(searchData?.artists[0]?.totalFollow) +
+                          " quan tâm") ||
+                        (searchData?.top?.objectType === "song" &&
+                          searchData?.top?.artistsNames)}
+                    </span>
+                  </div>
+                </Link>
+              )}
+              {searchData?.songs
+                ?.filter((item, index) =>
+                  [...Array(2).keys()].some((i) => i === index)
+                )
+                ?.map((item) => (
+                  <div
+                    key={item.encodeId}
+                    className="flex-1 bg-searchItem flex items-center rounded-lg hover:bg-bgIcon"
+                  >
+                    <SongItem
+                      key={item.encodeId}
+                      thumbnail={item.thumbnail}
+                      songId={item.encodeId}
+                      title={item.title}
+                      artists={item.artistsNames}
+                    />
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Bài Hát</h3>
+            <div className="flex justify-between gap-3 flex-wrap w-full">
+              {searchData?.songs?.map((item) => (
+                <div
                   key={item.encodeId}
-                  thumbnail={item.thumbnail}
-                  songId={item.encodeId}
-                  title={item.title}
-                  artists={item.artistsNames}
-                />
-              </div>
-            ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Bài Hát</h3>
-        <div className="flex justify-between gap-3 flex-wrap w-full">
-          {searchData?.songs?.map((item) => (
-            <div
-              key={item.encodeId}
-              className="flex-auto w-[40%] hover:bg-bgIcon cursor-pointer px-2 py-3 rounded-lg"
-            >
-              <ListSongItem songData={item} isHideAlbum />
+                  className="flex-auto w-[40%] hover:bg-bgIcon cursor-pointer px-2 py-3 rounded-lg"
+                >
+                  <ListSongItem songData={item} isHideAlbum />
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Playlist/Album</h3>
-        <div className="flex justify-between gap-3 flex-wrap ">
-          {searchData?.playlists?.slice(0, 5).map((item) => (
-            <SectionItem key={item.encodeId} item={item} />
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Nghệ Sĩ/OA</h3>
-        <div className="flex justify-between gap-3 flex-wrap ">
-          {searchData?.artists?.slice(0, 5).map((item) => (
-            <div className="py-5">
-              <Artist
-                key={item.id}
-                image={item.thumbnail}
-                title={item.name}
-                follow={item.totalFollow}
-                link={item.link}
-              ></Artist>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Playlist/Album</h3>
+            <div className="flex justify-between gap-3 flex-wrap ">
+              {searchData?.playlists?.slice(0, 5).map((item) => (
+                <SectionItem key={item.encodeId} item={item} />
+              ))}
             </div>
-          ))}
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Nghệ Sĩ/OA</h3>
+            <div className="flex justify-between gap-3 flex-wrap ">
+              {searchData?.artists?.slice(0, 5).map((item) => (
+                <div className="py-5" key={item.id}>
+                  <Artist
+                    image={item.thumbnail}
+                    title={item.name}
+                    follow={item.totalFollow}
+                    link={item.link}
+                  ></Artist>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="h-[200px]"></div>
         </div>
-      </div>
-      <div className="h-[200px]"></div>
-    </div>
+      )}
+      {!searchData?.top?.thumbnail && (
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-primary z-10 flex items-center justify-center">
+          <LoadingData />
+        </div>
+      )}
+    </>
   );
 };
 
--- src/containers/public/Singer.js
+++ src/containers/public/Singer.js
@@ -5,6 +5,7 @@ import ListSongItem from "../../components/ListSongItem";
 import SectionItem from "../../components/SectionItem";
 import Artist from "../../components/Artist";
 import parse from "html-react-parser";
+import LoadingData from "../../components/LoadingData";
 
 const Singer = () => {
   const {singer} = useParams();
@@ -25,121 +26,132 @@ const Singer = () => {
   }, [singer]);
 
   return (
-    <div className="flex flex-col w-full px-[59px] text-white">
-      <img
-        src={artistData?.cover}
-        alt=""
-        className="w-full h-[300px] object-cover rounded-lg"
-      />
-      <div>
-        <h1 className="text-[60px] font-bold py-5">{artistData?.name}</h1>
-        <div className="flex gap-5">
-          <span>
-            {new Intl.NumberFormat("en-US").format(artistData?.totalFollow)}{" "}
-            người quan tâm
-          </span>
-          <button
-            type="button"
-            className="bg-select px-6 py-1 rounded-l-full rounded-r-full flex items-center justify-center gap-1 text-[14px]"
-          >
-            QUAN TÂM
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Bài Hát</h3>
-        <div className="flex justify-between gap-3 flex-wrap w-full">
-          {artistData?.sections
-            ?.find((item) => item.sectionId === "aSongs")
-            ?.items?.map((item) => (
-              <div
-                key={item.encodeId}
-                className="flex-auto w-[40%] hover:bg-bgIcon cursor-pointer px-2 py-3 rounded-lg"
-              >
-                <ListSongItem songData={item} isHideAlbum />
-              </div>
-            ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Playlist/Album</h3>
-        <div className="flex justify-between gap-3 flex-wrap ">
-          {artistData?.sections
-            ?.find((item) => item.sectionId === "aSingle")
-            ?.items.slice(0, 5)
-            .map((item) => (
-              <div key={item.encodeId} className="flex-1">
-                <SectionItem item={item} />
-              </div>
-            ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Xuất Hiện Trong</h3>
-        <div className="flex gap-3 flex-wrap ">
-          {artistData?.sections
-            ?.find((item) => item.sectionId === "aPlaylist")
-            ?.items?.slice(0, 5)
-            .map((item) => (
-              <div className="flex-1">
-                <SectionItem key={item.encodeId} item={item} />
-              </div>
-            ))}
-        </div>
-      </div>
-      <div className="flex flex-col">
-        <h3 className="text-[20px] font-bold my-6">Có Thể Bạn Sẽ Thích</h3>
-        <div className="flex justify-between gap-3 flex-wrap ">
-          {artistData?.sections
-            ?.find((item) => item.sectionId === "aReArtist")
-            ?.items.map((item) => (
-              <div key={item.id} className="py-5">
-                <Artist
-                  image={item.thumbnail}
-                  title={item.name}
-                  follow={item.totalFollow}
-                  link={item.link}
-                ></Artist>
-              </div>
-            ))}
-        </div>
-      </div>
-      <div className="mt-6">
-        <h3 className="text-[20px] font-semibold">Về {artistData?.name}</h3>
-        <div className="flex gap-8 mt-6">
+    <>
+      {artistData?.cover && (
+        <div className="flex flex-col w-full px-[59px] text-white">
           <img
-            src={artistData?.thumbnailM}
+            src={artistData?.cover}
             alt=""
-            className="w-[500px] object-cover rounded-lg"
+            className="w-full h-[300px] object-cover rounded-lg"
           />
           <div>
-            <p
-              dangerouslySetInnerHTML={{__html: artistData?.biography}}
-              className="text-[14px] text-textGrey"
-            ></p>
-            <div className="flex py-10 gap-10">
-              <div className="flex flex-col">
-                <span className="text-[20px] font-bold">
-                  {new Intl.NumberFormat("en-US").format(
-                    artistData?.totalFollow
-                  ) || 0}
-                </span>
-                <span className="text-[14px] text-textGrey">
-                  Người quan tâm
-                </span>
-              </div>
-              <div className="flex flex-col">
-                <span className="text-[20px] font-bold">
-                  {artistData?.awards?.length || 0}
-                </span>
-                <span className="text-[14px] text-textGrey">Giải thưởng</span>
+            <h1 className="text-[60px] font-bold py-5">{artistData?.name}</h1>
+            <div className="flex gap-5">
+              <span>
+                {new Intl.NumberFormat("en-US").format(artistData?.totalFollow)}{" "}
+                người quan tâm
+              </span>
+              <button
+                type="button"
+                className="bg-select px-6 py-1 rounded-l-full rounded-r-full flex items-center justify-center gap-1 text-[14px]"
+              >
+                QUAN TÂM
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Bài Hát</h3>
+            <div className="flex justify-between gap-3 flex-wrap w-full">
+              {artistData?.sections
+                ?.find((item) => item.sectionId === "aSongs")
+                ?.items?.map((item) => (
+                  <div
+                    key={item.encodeId}
+                    className="flex-auto w-[40%] hover:bg-bgIcon cursor-pointer px-2 py-3 rounded-lg"
+                  >
+                    <ListSongItem songData={item} isHideAlbum />
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Playlist/Album</h3>
+            <div className="flex justify-between gap-3 flex-wrap ">
+              {artistData?.sections
+                ?.find((item) => item.sectionId === "aSingle")
+                ?.items.slice(0, 5)
+                .map((item) => (
+                  <div key={item.encodeId} className="flex-1">
+                    <SectionItem item={item} />
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Xuất Hiện Trong</h3>
+            <div className="flex gap-3 flex-wrap ">
+              {artistData?.sections
+                ?.find((item) => item.sectionId === "aPlaylist")
+                ?.items?.slice(0, 5)
+                .map((item) => (
+                  <div className="flex-1">
+                    <SectionItem key={item.encodeId} item={item} />
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <h3 className="text-[20px] font-bold my-6">Có Thể Bạn Sẽ Thích</h3>
+            <div className="flex justify-between gap-3 flex-wrap ">
+              {artistData?.sections
+                ?.find((item) => item.sectionId === "aReArtist")
+                ?.items.map((item) => (
+                  <div key={item.id} className="py-5">
+                    <Artist
+                      image={item.thumbnail}
+                      title={item.name}
+                      follow={item.totalFollow}
+                      link={item.link}
+                    ></Artist>
+                  </div>
+                ))}
+            </div>
+          </div>
+          <div className="mt-6">
+            <h3 className="text-[20px] font-semibold">Về {artistData?.name}</h3>
+            <div className="flex gap-8 mt-6">
+              <img
+                src={artistData?.thumbnailM}
+                alt=""
+                className="w-[500px] object-cover rounded-lg"
+              />
+              <div>
+                <p
+                  dangerouslySetInnerHTML={{__html: artistData?.biography}}
+                  className="text-[14px] text-textGrey"
+                ></p>
+                <div className="flex py-10 gap-10">
+                  <div className="flex flex-col">
+                    <span className="text-[20px] font-bold">
+                      {new Intl.NumberFormat("en-US").format(
+                        artistData?.totalFollow
+                      ) || 0}
+                    </span>
+                    <span className="text-[14px] text-textGrey">
+                      Người quan tâm
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span className="text-[20px] font-bold">
+                      {artistData?.awards?.length || 0}
+                    </span>
+                    <span className="text-[14px] text-textGrey">
+                      Giải thưởng
+                    </span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+          <div className="h-[200px]"></div>
         </div>
-      </div>
-      <div className="h-[200px]"></div>
-    </div>
+      )}
+      {!artistData?.cover && (
+        <div className="absolute top-0 bottom-0 left-0 right-0 bg-primary z-10 flex items-center justify-center">
+          <LoadingData />
+        </div>
+      )}
+    </>
   );
 };
 
